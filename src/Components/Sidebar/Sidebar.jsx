f03fea4ceb9e8e02b1493bc968d01ad6a51dc1d3
Satyakumarch <satyakumarchaudhary603@gmail.com>
2025-01-19 19:08:11 +0530

    updating the sidebar menu of the top level

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -7,10 +7,21 @@ const Sidebar = () => {
     <div className='sidebar'>
         <div className='top'>
             <img className='menu' src={assets.menu_icon}/>
+            <div>
+                 <img   src={assets.plus_icon}/>
+                <p>New Chat</p>
+            </div>
+            <div className="recent">
+                <p className='recent-title'>Recent</p>
+                <div className="recent-entry">
+                    <img src={assets.message_icon}/>
+                    <p>What is react...?</p>
 
-
+                </div>
+            </div>
         </div>
         <div className='buttom'>
+            
 
         </div>
     </div>
